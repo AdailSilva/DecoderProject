add5e9ac7e11c5aa2947ed81561891bddeb7c657
AdailSilva <ass8@bdmg.mg.gov.br>
2022-09-03 17:05:56 -0300

    Initial commit

---
+++ src/main/java/com/adailsilva/microservices/DecoderProjectApplication.java
@@ -0,0 +1,13 @@
+package com.adailsilva.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecoderProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecoderProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adailsilva/microservices/DecoderProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adailsilva.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecoderProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
